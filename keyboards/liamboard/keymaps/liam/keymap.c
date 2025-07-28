a2aa013d0df579304b90d6404e2f70e5b143ab70
Liam Fruzyna <liam@fruzyna.net>
2025-07-28 15:47:05 -0500

    Updated LiamBoard layout

--- keyboards/liamboard/keymaps/liam/keymap.c
+++ keyboards/liamboard/keymaps/liam/keymap.c
@@ -33,13 +33,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  |Numpad|Lower |    Space    |Raise | Left | Down | Up   |Right |
+ * | Ctrl | GUI  | Alt  |Numpad| Lower|    Space    | Raise| Left | Down | Up   | Right|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_5x12(
-   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
@@ -54,17 +54,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |   <  |   >  |   ?  |  \   |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      | INS  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      | Vol- | Vol+ |      |
+ * | CALC |      |      |      |      |             |      | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_5x12( \
   KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   KC_BSPC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,\
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LT  , KC_GT  , KC_QUES, KC_BSLS,\
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______ \
+  KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS, \
+  KC_CALC, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -74,59 +74,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |   <  |   >  |   ?  |  |   |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      | INS  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      | Vol- | Vol+ |      |
+ * | CALC |      |      |      |      |             |      | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_5x12( \
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   KC_BSPC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,\
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LT  , KC_GT  , KC_QUES, KC_PIPE,\
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______ \
+  KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS, \
+  KC_CALC, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |   /  |   *  |   -  |      |      |      |      |      | DEL  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |   7  |   8  |   9  |   +  |      | Left | Up   |Right |      | BSPC |
+ * | Tab  |      |   7  |   8  |   9  |   +  |      | Left | Up   | Right|      | BSPC |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |   4  |   5  |   6  |   +  |      | Left | Down |Right |      |      |
+ * | Tab  |      |   4  |   5  |   6  |   +  |      | Left | Down | Right|      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      | BSPC |   1  |   2  |   3  | ENT  |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | BSPC |   0  |   .  |      | ENT  |      |      |      |      |      |      |
+ * | CALC | BSPC |   0  |   .  |      | ENT  |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_5x12( \
-  _______, _______, _______, KC_SLSH, KC_ASTR, KC_MINS, _______, _______, _______, _______, _______, KC_DEL, \
-  _______, _______, KC_7   , KC_8   , KC_9   , KC_PLUS, _______, KC_LEFT, KC_UP  , KC_RGHT, _______, KC_BSPC, \
-  _______, _______, KC_4   , KC_5   , KC_6   , KC_PLUS, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
-  _______, KC_BSPC, KC_1   , KC_2   , KC_3   , KC_ENT , _______, _______, _______, _______, _______, _______, \
-  _______, KC_BSPC, KC_0   , KC_DOT , _______, KC_ENT , _______, _______, _______, _______, _______, _______ \
+  KC_NO,   KC_NO,   KC_NO,   KC_SLSH, KC_ASTR, KC_MINS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_DEL, \
+  KC_TAB,  KC_NO,   KC_7   , KC_8   , KC_9   , KC_PLUS, KC_NO,   KC_LEFT, KC_UP  , KC_RGHT, KC_NO,   KC_BSPC,\
+  KC_TAB,  KC_NO,   KC_4   , KC_5   , KC_6   , KC_PLUS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,  \
+  KC_NO,   KC_BSPC, KC_1   , KC_2   , KC_3   , KC_ENT , KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  \
+  KC_CALC, KC_BSPC, KC_0   , KC_DOT , KC_NO,   KC_ENT , KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO   \
 ),
 
 /* Numpad
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |   /  |   *  |   -  |      |      |      |      |      | DEL  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |   7  |   8  |   9  |   +  |      | Left | Up   |Right |      | BSPC |
+ * |      |      |   7  |   8  |   9  |   +  |      | Left | Up   | Right|      | BSPC |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |   4  |   5  |   6  |   +  |      | Left | Down |Right |      |      |
+ * |      |      |   4  |   5  |   6  |   +  |      | Left | Down | Right|      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      | BSPC |   1  |   2  |   3  | ENT  |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | BSPC |   0  |      |   .  | ENT  |      |      |      |      |      |NUMPAD|
+ * | CALC | BSPC |   0  |QWERTY|   .  | ENT  |      |      |      |      |      |QWERTY|
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMPAD] =  LAYOUT_ortho_5x12( \
-  _______, _______, _______, KC_SLSH, KC_ASTR, KC_MINS, _______, _______, _______, _______, _______, KC_DEL, \
-  _______, _______, KC_7   , KC_8   , KC_9   , KC_PLUS, _______, KC_LEFT, KC_UP  , KC_RGHT, _______, KC_BSPC, \
-  _______, _______, KC_4   , KC_5   , KC_6   , KC_PLUS, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
-  _______, KC_BSPC, KC_1   , KC_2   , KC_3   , KC_ENT , _______, _______, _______, _______, _______, _______, \
-  _______, KC_BSPC, KC_0   , QWERTY , KC_DOT , KC_ENT , _______, _______, _______, _______, _______, QWERTY \
+  KC_NO,   KC_NO,   KC_NO,   KC_SLSH, KC_ASTR, KC_MINS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_DEL, \
+  KC_NO,   KC_NO,   KC_7   , KC_8   , KC_9   , KC_PLUS, KC_NO,   KC_LEFT, KC_UP  , KC_RGHT, KC_NO,   KC_BSPC,\
+  KC_NO,   KC_NO,   KC_4   , KC_5   , KC_6   , KC_PLUS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,  \
+  KC_NO,   KC_BSPC, KC_1   , KC_2   , KC_3   , KC_ENT , KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  \
+  KC_CALC, KC_BSPC, KC_0   , QWERTY , KC_DOT , KC_ENT , KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   QWERTY  \
 )
 
 };
